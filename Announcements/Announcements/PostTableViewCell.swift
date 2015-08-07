b7eb101999f52cfe2e61fff4ffed5db190546852
adityachugh <developer@adityachugh.com>
2015-08-07 13:35:55 -0400

    Updated TodayTableViewController

--- Announcements/Announcements/PostTableViewCell.swift
+++ Announcements/Announcements/PostTableViewCell.swift
@@ -10,15 +10,28 @@ import UIKit
 
 class PostTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var view: UIView!
+    @IBOutlet weak var label: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
-
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        NSBundle.mainBundle().loadNibNamed("PostTableViewCell", owner: self, options: nil)
+        self.addSubview(view)
+        setup()
+    }
+    
+    func setup() {
+        label.text = "Hello"
+    }
 }
--- Announcements/Announcements/TodayTableViewController.swift
+++ Announcements/Announcements/TodayTableViewController.swift
@@ -11,24 +11,28 @@ import UIKit
 class TodayTableViewController: UITableViewController {
     
     override func viewDidLoad() {
-        
+        self.tableView.registerNib(UINib(nibName: "PostTableViewCell", bundle: nil), forCellReuseIdentifier: "Post")
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
     }
     
-//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        
-//    }
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
     
-//    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-//        
-//    }
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Post") as! PostTableViewCell
+        cell.label.text = "Hello"
+        
+        return cell
+    }
     
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return UIStatusBarStyle.LightContent
